e4e4d15dcd0dfdef7feb457c4c05aaec194efd16
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-08-24 17:14:48 +0500

    We have added an input

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const minute = document.getElementById("minute");
 const second = document.getElementById("second");
 const startBtn = document.getElementById("startBtn");
-let m = 24;
+const input = document.getElementById("input");
+
+let m = input;
 let s = 60;
 
 const startTimer = () => {
   setInterval(() => {
     if (s > 0) {
       s--;
-      m;
+      m.value;
     } else if (s <= 0) {
       s = 59;
-      m--;
+      m.value--;
     }
 
-    minute.innerHTML = m;
+    minute.innerHTML = m.value;
     second.innerHTML = s < 10 ? `0${s}` : s;
   }, 1000);
 };
