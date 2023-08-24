02889eba9a1ee3a2dca65fc9b30b1327a0a0e322
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-08-24 17:30:25 +0500

    Date added

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const minute = document.getElementById("minute");
 const second = document.getElementById("second");
 const startBtn = document.getElementById("startBtn");
 const input = document.getElementById("input");
+const todaysDate = document.getElementById("todaysDate");
+const date = new Date();
+
+console.log(date);
+
+todaysDate.innerHTML = `Todays is ${date.getUTCDate()} of august`;
 
 let m = input;
 let s = 60;
 
 const startTimer = () => {
   setInterval(() => {
-    if (s > 0) {
+    if (s > 0 && m.value > 0) {
       s--;
       m.value;
-    } else if (s <= 0) {
+    } else if (s <= 0 && m.value > 0) {
       s = 59;
       m.value--;
     }
