30a67d9a7f1e025e5011ecb5be5a515389052b87
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-08-28 10:42:54 +0500

    disabled added

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const startBtn = document.getElementById("startBtn");
 const input = document.getElementById("input");
 const todaysDate = document.getElementById("todaysDate");
 const date = new Date();
+// startBtn.disabled = false;
 
-console.log(date);
+// console.log(date);
 
 todaysDate.innerHTML = `Today is ${date.getUTCDate()} of august`;
 
@@ -28,5 +29,10 @@ const startTimer = () => {
 
     minute.innerHTML = m.value - 1 < 0 ? 0 : m.value - 1;
     second.innerHTML = s < 10 ? `0${s}` : s;
+
+    localStorage.setItem("minute", m.value - 1);
+    localStorage.setItem("second", s);
   }, 1000);
+
+  startBtn.setAttribute("disabled", "!disabled");
 };
